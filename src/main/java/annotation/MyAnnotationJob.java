0b67d06917e45679c4fb1c10fbcf70b041992acd
bruce.chen <bruce.chen@yxunistar.com>
2018-08-25 16:40:49 +0800

    delete don't need code

--- src/main/java/annotation/MyAnnotationJob.java
+++ src/main/java/annotation/MyAnnotationJob.java
@@ -1,16 +1,20 @@
 package annotation;
 
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.springframework.scheduling.quartz.QuartzJobBean;
 import org.springframework.stereotype.Component;
 
 @Component
 //@PersistJobDataAfterExecution
 //@DisallowConcurrentExecution
-public class MyAnnotationJob {// extends QuartzJobBean {
-//	protected void executeInternal(JobExecutionContext ctx) throws JobExecutionException {
-//		System.out.println("xxxxxxxxxxxxxxxxxxxxx");
-//	}
+public class MyAnnotationJob extends QuartzJobBean {
+	@Override
+	protected void executeInternal(JobExecutionContext ctx) throws JobExecutionException {
+		System.out.println("xxxxxxxxxxxxxxxxxxxxx");
+	}
 
-	 public void getXxx() {
-		 System.out.println("xxxxxxxxxxxxxxxxxxxxx");
-	 }
+//	 public void getXxx() {
+//		 System.out.println("xxxxxxxxxxxxxxxxxxxxx4");
+//	 }
 }
--- src/main/java/annotation/QuartzConfiguration.java
+++ src/main/java/annotation/QuartzConfiguration.java
@@ -1,26 +1,35 @@
 package annotation;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.quartz.MethodInvokingJobDetailFactoryBean;
+import org.springframework.scheduling.quartz.CronTriggerFactoryBean;
+import org.springframework.scheduling.quartz.JobDetailFactoryBean;
 import org.springframework.scheduling.quartz.SchedulerFactoryBean;
-import org.springframework.scheduling.quartz.SimpleTriggerFactoryBean;
+
 
 @Configuration
 @ComponentScan("annotation")
 public class QuartzConfiguration {
-	@Autowired
-	private MyAnnotationJob myJob;
+//	@Autowired
+//	private MyAnnotationJob myJob;
+
+	@Bean
+	public JobDetailFactoryBean jobDetail() {
+		JobDetailFactoryBean job = new JobDetailFactoryBean();
+		job.setJobClass(MyAnnotationJob.class);
+		return job;
+	}
 
 	// @Bean
-	// public JobDetailFactoryBean jobDetail() {
-	// JobDetailFactoryBean job = new JobDetailFactoryBean();
-	// job.setJobClass(MyJob.class);
+	// public MethodInvokingJobDetailFactoryBean jobDetail() {
+	// MethodInvokingJobDetailFactoryBean job = new
+	// MethodInvokingJobDetailFactoryBean();
+	// job.setTargetObject(myJob);
+	// job.setTargetMethod("getXxx");
 	// return job;
 	// }
-	//
+
 	// @Bean
 	// public SimpleTriggerFactoryBean simpleTrigger() {
 	// SimpleTriggerFactoryBean trigger = new SimpleTriggerFactoryBean();
@@ -29,43 +38,19 @@ public class QuartzConfiguration {
 	// trigger.setRepeatInterval(2000);
 	// return trigger;
 	// }
-	//
-	// @Bean
-	// public SchedulerFactoryBean schedulerFactory() {
-	// SchedulerFactoryBean schedule = new SchedulerFactoryBean();
-	// schedule.setTriggers(simpleTrigger().getObject());
-	// return schedule;
-	// }
 
 	@Bean
-	public MethodInvokingJobDetailFactoryBean jobDetail() {
-		MethodInvokingJobDetailFactoryBean job = new MethodInvokingJobDetailFactoryBean();
-		job.setTargetObject(myJob);
-		job.setTargetMethod("getXxx");
-		return job;
+	public CronTriggerFactoryBean cronTriggerBean() {
+		CronTriggerFactoryBean cronTriggerFactoryBean = new CronTriggerFactoryBean();
+		cronTriggerFactoryBean.setJobDetail(jobDetail().getObject());
+		cronTriggerFactoryBean.setCronExpression("*/2 * * * * ?");
+		return cronTriggerFactoryBean;
 	}
 
 	@Bean
-	public SimpleTriggerFactoryBean simpleTrigger() {
-		SimpleTriggerFactoryBean trigger = new SimpleTriggerFactoryBean();
-		trigger.setJobDetail(jobDetail().getObject());
-		trigger.setStartDelay(10);
-		trigger.setRepeatInterval(2000);
-		return trigger;
-	}
-
-	// @Bean
-	// public CronTriggerFactoryBean cronTriggerBean() {
-	// CronTriggerFactoryBean cronTriggerFactoryBean = new CronTriggerFactoryBean();
-	// cronTriggerFactoryBean.setJobDetail(jobDetail().getObject());
-	// cronTriggerFactoryBean.setCronExpression("* * * * * ?");
-	// return cronTriggerFactoryBean;
-	// }
-
-	@Bean
-	public SchedulerFactoryBean trigger() {
+	public SchedulerFactoryBean schedulerFactory() {
 		SchedulerFactoryBean schedule = new SchedulerFactoryBean();
-		schedule.setTriggers(simpleTrigger().getObject());
+		schedule.setTriggers(cronTriggerBean().getObject());
 		return schedule;
 	}
 
